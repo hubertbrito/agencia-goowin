26b5383714ef5d63486418c802f8ee85348709a4
Hubert369 <86267581+hubertbrito@users.noreply.github.com>
2024-01-09 16:24:47 -0300

    Add files via upload
    Novo designer do site remodelado

--- app.js
+++ app.js
@@ -1,46 +1,51 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var themeButton = document.getElementById('theme-button');
-  
-    themeButton.addEventListener('click', function() {
-        document.body.classList.toggle('dark-mode');
-  
-        localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
-  
-        alterarTema(); // Atualize as imagens e o texto no botão
-    });
-  
-    // Carregue o tema preferido do localStorage
-    var preferredTheme = localStorage.getItem('theme');
-    if (preferredTheme === 'dark') {
-        document.body.classList.add('dark-mode');
-        alterarTema(); // Atualize as imagens e o texto no botão
-    }
-  });
-  
-  function alterarTema() {
-    var button = document.getElementById('theme-button');
-    var image = document.getElementById('theme-icon');
-    var logo = document.getElementById('topo-logo');
-    var text = document.getElementById('theme-text');
-    var menu = document.getElementById('header');
-    var link = document.getElementById('link-cor');  
-  
-    if (document.body.classList.contains('dark-mode')) {
-        button.className = 'modo-escuro';
-        image.src = 'assets/sun.png';
-        logo.src = 'assets/logo2.png';
-        text.textContent = 'Light';
-        menu.className = 'menu-dark';
-        link.className = 'link-branco';
-    } else {
-        button.className = 'modo-claro';
-        image.src = 'assets/moon.png';
-        logo.src = 'assets/logoclara.png';
-        text.textContent = 'Dark';
-        menu.className = 'menu-light';
-        link.className = 'link-preto';
-    }
+  var themeButton = document.getElementById('theme-button');
+
+  // Defina o tema escuro como padrão
+  if (!localStorage.getItem('theme')) {
+      document.body.classList.add('dark-mode');
   }
+
+  themeButton.addEventListener('click', function() {
+      document.body.classList.toggle('dark-mode');
+
+      localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
+
+      alterarTema(); // Atualize as imagens e o texto no botão
+  });
+
+  // Carregue o tema preferido do localStorage
+  var preferredTheme = localStorage.getItem('theme');
+  if (preferredTheme === 'light') {
+      document.body.classList.remove('dark-mode');
+  } else {
+      document.body.classList.add('dark-mode');
+  }
+  alterarTema(); // Atualize as imagens e o texto no botão
+});
+
+function alterarTema() {
+  var button = document.getElementById('theme-button');
+  var image = document.getElementById('theme-icon');
+  var logo = document.getElementById('topo-logo');
+  var text = document.getElementById('theme-text');
+  var menu = document.getElementById('header');
+
+  if (document.body.classList.contains('dark-mode')) {
+      button.className = 'modo-escuro';
+      image.src = 'assets/sun.png';
+      logo.src = 'assets/logo-clara-bola-roxa.png';
+      text.textContent = 'Light';
+      menu.className = 'menu-dark';
+  } else {
+      button.className = 'modo-claro';
+      image.src = 'assets/moon.png';
+      logo.src = 'assets/logo-preta-bola-roxa.png';
+      text.textContent = 'Dark';
+      menu.className = 'menu-light';
+  }
+}
+
   
   
   
